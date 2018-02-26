8717b5d2be86732cfa71c2b5ae85630828621ebc
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-02-26 21:24:02 +0100

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,58 @@
+/*
+ * The Circle class models a circle with a radius and color.
+ */
+public class Circle {  // Save as "Circle.java"
+    // private instance variable, not accessible from outside this class
+    private double radius;
+    private String color;
+
+    // The default constructor with no argument.
+    // It sets the radius and color to their default value.
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    // 2nd constructor with given radius, but color default
+    public Circle(double r) {
+        this.radius = r;
+        this.color = "red";
+    }
+
+    // 3rd constructor to construct a new instance of Circle with the given radius and color
+    public Circle (double r, String c) {
+        this.radius = r;
+        this.color = c;
+    }
+
+    // A public method for retrieving the radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // A public method for retrieving the color
+    public String getColor() {
+        return color;
+    }
+
+    // A public method for computing the area of circle
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+
+    // Setter for instance variable radius
+    public void setRadius(double newRadius) {
+        this.radius = newRadius;
+    }
+
+    // Setter for instance variable color
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    // Return a description of this instance in the form of
+    // Circle[radius=r,color=c]
+    public String toString() {
+        return "Circle[radius=" + radius + " color=" + color + "]";
+    }
+}
---
+++ src/TestCircle.java
@@ -0,0 +1,65 @@
+public class TestCircle {  // Save as "TestCircle.java"
+
+    public static void main(String[] args) {
+
+        // Declare an instance of Circle class called c1.
+        // Construct the instance c1 by invoking the "default" constructor
+        // which sets its radius and color to their default value.
+
+        Circle c1 = new Circle();
+
+        // Invoke public methods on instance c1, via dot operator.
+
+        System.out.println("The circle has radius of "
+                + c1.getRadius() + " and area of " + c1.getArea());
+
+        // Declare an instance of class circle called c2.
+        // Construct the instance c2 by invoking the second constructor
+        // with the given radius and default color.
+
+        Circle c2 = new Circle(2.0);
+
+        // Invoke public methods on instance c2, via dot operator.
+
+        System.out.println("The circle has radius of "
+                + c2.getRadius() + " and area of " + c2.getArea());
+
+        // Modify the test program TestCircle to construct an instance of Circle using this constructor.
+
+        // Deklarer c3 som en instance af klassen Circle og kalder på dens 3 constructor med en radius og color.
+        Circle c3 = new Circle(3.0,"blue");
+
+        // Kalder på c3's public methods via dot operator.
+        System.out.println("The circle has radius of "
+                + c3.getRadius() + " and color " + c3.getColor());
+
+        /*
+        * public vs. private: In TestCircle, can you access the instance variable radius directly (e.g., System.out.println(c1.radius));
+        * or assign a new value to radius (e.g., c1.radius=5.0)? Try it out and explain the error messages.
+         */
+
+        // System.out.println(c1.radius);
+
+        // Giver error message: Error:(41, 30) java: radius has private access in Circle
+        // Den siger at man ikke kan få adgang til den fordi den har en private access, man ville kunne få adgang til den på den måde hvis den var protected eller public.
+
+        // construct an instance of Circle
+        Circle c4 = new Circle();
+
+        // change radius
+        c4.setRadius(5.0);
+
+        // Print radius via getter
+        System.out.println("radius is: " + c4.getRadius());
+
+        // Change color
+        c4.setColor("black");
+
+        // Print color via getter
+        System.out.println("color is: " + c4.getColor());
+
+        Circle c5 = new Circle(5.0);
+
+        System.out.println(c5.toString()); // explicit call
+    }
+}
