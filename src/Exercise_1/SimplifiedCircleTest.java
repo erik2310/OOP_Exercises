d1abb9cdcf96a13b13215c887d977e798b5df536
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-02-28 21:32:53 +0100

    Lavet SimplifiedCircleTest klassen.

---
+++ src/Exercise_1/SimplifiedCircleTest.java
@@ -0,0 +1,27 @@
+package Exercise_1;
+
+public class SimplifiedCircleTest {
+
+    public static void main(String[] args) {
+
+        // Laver et SimplifiedCircle objekt ved at kalde på dens constructor.
+        SimplifiedCircle circle = new SimplifiedCircle();
+
+        // Sætter circlens radius til 5.0
+        circle.setRadius(5.0);
+
+        // Henter cirklens radius og udskriver det
+        System.out.println(circle.getRadius());
+
+        // Henter dens areal og udskriver det
+        System.out.println(circle.getArea());
+
+        // Henter dens omkreds og udskriver det
+        System.out.println(circle.getCircumference());
+
+        // Udskriver dens og udskriver det
+        System.out.println(circle.toString());
+
+    }
+
+}
